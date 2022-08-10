b0d450b1720004b9f3644058350ceb2714a2ee89
Vishwas Tarende <vishwastarende@Vishwass-MacBook-Pro.local>
2022-08-10 16:38:25 +0530

    02

---
+++ 02_array_Traversal_UsingRangeBased.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+   
+    int arr[] = {10,20,30,40,50};
+    
+    for(int x : arr) {
+        cout<< x << " Hmm ";
+    }
+
+
+
+    return 0;
+}
