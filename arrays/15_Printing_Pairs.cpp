7211108e03e411a0e1bf78319280901dffaae328
Vishwas Tarende <vishwastarende@Vishwass-MacBook-Pro.local>
2022-08-24 17:13:47 +0530

    add

---
+++ arrays/15_Printing_Pairs.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void printing_Pairs(int arr[], int size) {
+
+    for(int i = 0; i < size; i++) {
+        for(int j = i+1; j < size; j++) {
+            cout<<arr[i] << "," << arr[j] << " " <<endl;
+        }
+
+        cout<<endl;
+        
+    }
+    
+
+}
+
+int main() {
+
+    int arr[] = {1,2,3,4};
+    int size = sizeof(arr) / sizeof(int);
+    printing_Pairs(arr,size);
+
+
+
+
+
+    return 0;
+}
